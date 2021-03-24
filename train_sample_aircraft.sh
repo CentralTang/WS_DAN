33131cdf76ef31c3d6023ee95e6af7dad43a9fd3
CentralTang <yiqiao1996@gmail.com>
2021-03-24 09:30:35 +0800

    ...

--- train_sample_aircraft.sh
+++ train_sample_aircraft.sh
@@ -6,14 +6,14 @@ MODEL_PATH='./pre_trained/inception_v3.ckpt'
 
 python train_sample.py --learning_rate=0.001 \
                             --dataset_name=$DATASET \
-                            --dataset_dir="./$DATASET/Data/tfrecords" \
+                            --dataset_dir="../Data/$DATASET/Data/tfrecords" \
                             --train_dir=$TRAIN_DIR \
                             --checkpoint_path=$MODEL_PATH \
                             --max_number_of_steps=80000 \
                             --weight_decay=1e-5 \
                             --model_name='inception_v3_bap' \
                             --checkpoint_exclude_scopes="InceptionV3/bilinear_attention_pooling" \
-                            --batch_size=12 \
+                            --batch_size=8 \
                             --train_image_size=448 \
                             --num_clones=1 \
                             --gpus="0"\
